ed95f07843ca7424575bb5e25dbfccf6ca073055
kimkuang <ygtq_kuang@163.com>
2023-02-02 17:51:33 +0800

    first init

---
+++ elpa/29.0/develop/helm-core-20230111.1831/helm-core-pkg.el
@@ -0,0 +1,11 @@
+(define-package "helm-core" "20230111.1831" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.7"))
+  :commit "b978aee0f0edce350d42122c1e6fe73f11243a19" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
